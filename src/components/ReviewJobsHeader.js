3f21fe82fc758fbec2727403ea9dbbd630719d46
adjohari <adjohari25@gmail.com>
2024-07-23 11:09:51 +0530

    Style Header of Review Jobs

--- src/components/ReviewJobsHeader.js
+++ src/components/ReviewJobsHeader.js
@@ -1,25 +1,40 @@
 import * as React from "react";
-import { Text, View } from "react-native";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import { Button } from "react-native-elements";
+
+//Platform helps to identify which platform the application is running on : ios, android, windows etc
 
 export default ({ navigation }) => {
   return (
     <View
       style={{
+        marginTop: Platform.OS === "android" ? 24 : 0,
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
         padding: 10,
-        backgroundColor: "#f4511e",
       }}
     >
-      <Text style={{ color: "#fff", fontSize: 18 }}>Review Jobs</Text>
+      <Text style={{ color: "black", fontWeight: "bold", fontSize: 18 }}>
+        Review Jobs
+      </Text>
       <Button
         title="Settings"
         onPress={() => {
           navigation.navigate("Settings");
         }}
+        buttonStyle={styles.buttonStyle}
+        titleStyle={styles.titleStyle}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  buttonStyle: {
+    backgroundColor: "rgba(0,0,0,0)",
+    borderColor: "black",
+    borderWidth: 2,
+  },
+  titleStyle: { color: "black" },
+});
